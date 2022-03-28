45a6b498dcccbc5e34c23f0f40c0a401a60bf49d
Isha Gosalia <ishagosalia1@gmail.com>
2022-03-28 15:40:00 -0700

    Initial Commit

---
+++ MedicAgent WebApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MedicAgent WebApp
+//
+//  Created by Isha Gosalia on 3/28/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
