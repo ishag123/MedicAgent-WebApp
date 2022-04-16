8cc7289b930051793da9ab350a92ffb85802aa4d
Isha Gosalia <ishagosalia1@gmail.com>
2022-04-15 19:24:18 -0700

    app stuff

---
+++ MedicAgent WebApp/LowOnStockViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LowOnStockViewController.swift
+//  MedicAgent WebApp
+//
+//  Created by Isha Gosalia on 4/15/22.
+//
+
+import Cocoa
+
+class LowOnStockViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    @IBOutlet weak var checkBoxOne: NSButton!
+    
+    @IBOutlet weak var checkBoxTwo: NSButton!
+    
+    @IBOutlet weak var checkBoxThree: NSButton!
+    
+    
+    @IBAction func updateButton(_ sender: NSButton) {
+        if checkBoxOne.state  == .on{
+            checkBoxOne.isHidden = true
+        } else {
+            checkBoxOne.isHidden = false
+        }
+        
+        if checkBoxTwo.state == .on{
+            checkBoxTwo.isHidden = true
+        } else {
+            checkBoxTwo.isHidden = false
+        }
+        
+        if checkBoxThree.state == .on{
+            checkBoxThree.isHidden = true
+        } else {
+            checkBoxThree.isHidden = false
+        }
+        
+    }
+    
+}
---
+++ MedicAgent WebApp/ProductsInTransitViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsInTransitViewController.swift
+//  MedicAgent WebApp
+//
+//  Created by Isha Gosalia on 4/15/22.
+//
+
+import Cocoa
+
+class ProductsInTransitViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
